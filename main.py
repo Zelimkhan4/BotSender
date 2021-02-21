9dd2f6752b139df36182a90b94d3e3a753fae683
Zelimkhan4 <vip.guchiev@mail.ru>
2021-02-21 14:15:17 +0300

    Добавил класс группы.

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import pprint
 
 bot = telebot.TeleBot(config.apikey)
 
+class Group:
+    def __init__(name, participants):
+        self.name = name
+        self.participants = participants
+
+    def add_participant(participant):
+        self.participants.append(participant)
 
 @bot.message_handler(content_types=['text'])
 def send_message(message):
